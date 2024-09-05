0a927808759aba122f9d156a523f20bd5d7a183c
Thiago Maran <thiagobiazusmaran@gmail.com>
2024-09-05 20:17:47 -0300

    posgresql

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -14,7 +14,7 @@ if os.getenv("DEBUG") == 0: # '0' como string, pois getenv retorna strings
     link_banco = os.getenv("DATABASE_URL")
 else:
     link_banco = "sqlite:///comunidade.db"
-app.config["SQLALCHEMY_DATABASE_URI"] = link_banco
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SECRET_KEY"] = "f7d51183b5b7ec5005c5d66d9fa887f4"
 app.config["UPLOAD_FOLDER"] = "static/fotos_post"
 
