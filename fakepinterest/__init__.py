e67bb1941155554b88b0ca3fc5ac437b1d4b16f8
Thiago Maran <thiagobiazusmaran@gmail.com>
2024-09-06 14:29:55 -0300

    aaa

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -11,12 +11,12 @@ app = Flask(__name__)
 
 # Verifica se o ambiente é de desenvolvimento ou produção
 
-if os.getenv("DEBUG") == 0: # '0' como string, pois getenv retorna strings
+'''if os.getenv("DEBUG") == 0: # '0' como string, pois getenv retorna strings
     link_banco = os.getenv("DATABASE_URL")
 else:
-    link_banco = "sqlite:///comunidade.db"
+    link_banco = "sqlite:///comunidade.db"'''
 
-app.config["SQLALCHEMY_DATABASE_URI"] = link_banco
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SECRET_KEY"] = "f7d51183b5b7ec5005c5d66d9fa887f4"
 app.config["UPLOAD_FOLDER"] = "static/fotos_post"
 
